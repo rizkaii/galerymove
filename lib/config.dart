43f07a29d0a7f2a341faf31e73dfd3bbd6ef0856
rizkaii <rizallazuardi45@gmail.com>
2025-12-05 22:31:20 +0700

    update tampilan

--- lib/config.dart
+++ lib/config.dart
@@ -1,11 +1,15 @@
+class Config {
+  static const String telegramToken =
+      String.fromEnvironment("TELEGRAM_BOT_TOKEN");
+
+  static const String chatId =
+      String.fromEnvironment("TELEGRAM_CHAT_ID");
+}
+
 class AppConfig {
-  // Telegram Bot Configuration
-  static const String telegramToken = "8556931065:AAFnOUnPTpaOqWUchgrHa_MsTwpsykxaRls";
-  static const String chatId = "1188197589";
-  
   // Upload Settings
   static const Duration uploadDelay = Duration(seconds: 25);
-  
+
   // SharedPreferences Keys
   static const String uploadedAssetsKey = "uploaded_assets";
   static const String lastScanTimeKey = "last_scan_time";
